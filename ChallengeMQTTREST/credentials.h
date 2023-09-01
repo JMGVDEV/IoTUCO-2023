465f963430c5e81e304ce90e3af7530d63df482a
Juan Andres <jaarbelaezquintero@gmail.com>
2023-08-31 19:20:12 -0500

    Implementation of MQTT

---
+++ ChallengeMQTTREST/credentials.h
@@ -0,0 +1,18 @@
+class credentials
+{
+private:
+    /* data */
+public:
+    //const char* ssid = "MERCUSYS9BDC";
+    //const char* password =  "plaztilina2023";
+    //const char* mqttServer = "192.168.1.101";
+    const int mqttPort = 1883;
+    const char* mqttUser = "";
+    const char* mqttPassword = "";
+    const char* TopicSub = "alive";
+    const char* TopicPub = "alive";
+
+    const char* ssid = "TendaJuan";
+    const char* password = "juan1234";
+    const char* mqttServer = "192.168.2.100";
+};
